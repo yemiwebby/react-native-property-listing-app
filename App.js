fe6c3ce6bcb56cd518d5281ea928e67f34e140cd
yemiwebby <yemiwebby@gmail.com>
2018-04-11 12:03:38 +0100

    Initial commit

---
+++ App.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+'use strict';
+import React, { Component } from 'react';
+import { StackNavigator, } from 'react-navigation';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import SearchPage from './searchPage';
+import SearchResults from './SearchResults';
+
+const App = StackNavigator({
+  Home: { screen: SearchPage },
+  Results: { screen: SearchResults },
+});
+export default App;
