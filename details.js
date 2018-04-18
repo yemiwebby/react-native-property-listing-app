e804cf37409d8411731d34fbf50d4cf2cb7d4329
yemiwebby <yemiwebby@gmail.com>
2018-04-18 17:20:11 +0100

    Update the content of property detials view

---
+++ details.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Text, StyleSheet, View } from 'react-native'
+
+export default class DetailsComponent extends Component {
+  render() {
+    return (
+      <View>
+        <Text> Individual component details  </Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({})
